06d4a75a9df8b26da79faceb36fb9a21cbd4b750
cenkcorapci <cenk.corapci@gmail.com>
2019-11-18 09:46:47 +0300

    CLI parameters and readme

--- experiment.py
+++ experiment.py
@@ -1,4 +1,6 @@
+import argparse
 import logging
+import pathlib
 
 import gym
 import numpy as np
@@ -10,14 +12,39 @@ from ai.ppo import PPO
 from config import *
 from data.memory import Memory
 
-summary_writer = SummaryWriter(log_dir=TB_LOGS_PATH)
-logging.info('Tensorboard path: {}'.format(TB_LOGS_PATH))
+parser = argparse.ArgumentParser()
+parser.add_argument("--env_name", help="gym environment to be used", type=str, default='BipedalWalker-v2')
+parser.add_argument("--render", help="render gym environment", type=bool, default=False)
+parser.add_argument("--solved_reward", help="stop training if avg_reward > solved_reward", type=int, default=300)
+parser.add_argument("--log_interval", help="print avg reward in the interval", type=int, default=20)
+parser.add_argument("--max_episodes", help="max training episodes", type=int, default=10000)
+parser.add_argument("--max_timesteps", help="max timesteps in one episode", type=int, default=1500)
+parser.add_argument("--update_timestep", help="max timesteps in one episode", type=int, default=4000)
+parser.add_argument("--action_std", help="constant std for action distribution (Multivariate Normal)", type=float,
+                    default=.5)
+parser.add_argument("--K_epochs", help="update policy for K epochs", type=int, default=80)
+parser.add_argument("--eps_clip", help="clip parameter for PPO", type=float, default=.2)
+parser.add_argument("--gamma", help="discount factor", type=float, default=.99)
+parser.add_argument("--lr", help="learning rate", type=float, default=.0003)
+parser.add_argument("--betas", type=tuple, default=(.9, .999))
+parser.add_argument("--log_path", help="Tensorboard log path", type=str, default='tb_logs')
+parser.add_argument("--model_path", help="Path for model persistence", type=str, default='models')
+
+args = parser.parse_args()
+
+parser.log_path = "{parser.log_path}/{EXPERIMENT_NAME}"
+
+pathlib.Path(args.log_path).mkdir(parents=True, exist_ok=True)
+pathlib.Path(args.model_path).mkdir(parents=True, exist_ok=True)
+
+summary_writer = SummaryWriter(log_dir=args.log_path)
+logging.info(f"Tensorboard path: {args.log_path}")
 
 # if gpu is to be used
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # creating environment
-env = gym.make(env_name)
+env = gym.make(args.env_name)
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.shape[0]
 
@@ -28,8 +55,8 @@ if random_seed:
     np.random.seed(random_seed)
 
 memory = Memory()
-ppo = PPO(device, state_dim, action_dim, action_std, lr, betas, gamma, K_epochs, eps_clip)
-print(lr, betas)
+ppo = PPO(device, state_dim, action_dim, args.action_std, args.lr, betas, args.gamma, args.K_epochs, args.eps_clip)
+print(args.lr, betas)
 
 # logging variables
 running_reward = 0
@@ -37,10 +64,10 @@ avg_length = 0
 time_step = 0
 
 # training loop
-for i_episode in tqdm(range(1, max_episodes + 1), desc='Training'):
+for i_episode in tqdm(range(1, args.max_episodes + 1), desc='Training'):
     state = env.reset()
 
-    for t in tqdm(range(max_timesteps), desc='Running environment'):
+    for t in tqdm(range(args.max_timesteps), desc='Running environment'):
         time_step += 1
         # Running policy_old:
         action = ppo.select_action(state, memory)
@@ -51,12 +78,12 @@ for i_episode in tqdm(range(1, max_episodes + 1), desc='Training'):
         memory.is_terminals.append(done)
 
         # update if its time
-        if time_step % update_timestep == 0:
+        if time_step % args.update_timestep == 0:
             ppo.update(memory)
             memory.clear_memory()
             time_step = 0
         running_reward += reward
-        if render:
+        if args.render:
             env.render()
         if done:
             break
@@ -64,23 +91,23 @@ for i_episode in tqdm(range(1, max_episodes + 1), desc='Training'):
     avg_length += t
 
     # stop training if avg_reward > solved_reward
-    if running_reward > (log_interval * solved_reward):
+    if running_reward > (args.log_interval * args.solved_reward):
         print("########## Solved! ##########")
-        torch.save(ppo.policy.state_dict(), './PPO_continuous_solved_{}.pth'.format(env_name))
+        torch.save(ppo.policy.state_dict(), './PPO_continuous_solved_{}.pth'.format(args.env_name))
         break
 
     # save every 500 episodes
     if i_episode % 50 == 0:
-        torch.save(ppo.policy.state_dict(), '{}PPO_{}.pth'.format(MODEL_PATH, env_name))
+        torch.save(ppo.policy.state_dict(), '{}PPO_{}.pth'.format(args.MODEL_PATH, args.env_name))
 
     # logging
     summary_writer.add_scalar('avg episode length', avg_length, i_episode)
     summary_writer.add_scalar('reward', running_reward, i_episode)
     summary_writer.close()
 
-    if i_episode % log_interval == 0:
-        avg_length = int(avg_length / log_interval)
-        running_reward = int((running_reward / log_interval))
+    if i_episode % args.log_interval == 0:
+        avg_length = int(avg_length / args.log_interval)
+        running_reward = int((running_reward / args.log_interval))
 
         print('Episode {} \t Avg length: {} \t Avg reward: {}'.format(i_episode, avg_length, running_reward))
         running_reward = 0
--- readme.md
+++ readme.md
@@ -1,2 +1,32 @@
+# PPO-Agent
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+![GitHub last commit](https://img.shields.io/github/last-commit/cenkcorapci/ppo-agent)
+[![Requirements Status](https://requires.io/github/cenkcorapci/ppo-agent/requirements.svg?branch=master)](https://requires.io/github/cenkcorapci/ppo-agent/requirements/?branch=master)
+
 A [Proximal Policy Optimization ](https://arxiv.org/abs/1707.06347)
  implementation with [PyTorch](https://pytorch.org).
+ 
+## Usage
+To train on [BipedalWalker-v2](https://gym.openai.com/envs/BipedalWalker-v2/) with default parameters use;
+```bash
+python experiment.py
+```
+Here are the optional parameters;
+
+|parameter name| description|type|default|
+|----|---|---|----|
+|--env_name|gym environment to be used|str|BipedalWalker-v2|
+|--render|render gym environment|bool|False|
+|--solved_reward|stop training if avg_reward > solved_reward|int|300|
+|--log_interval|print avg reward in the interval|int|20|
+|--max_episodes|max training episodes|int|10000|
+|--max_timesteps|max timesteps in one episode|int|1500|
+|--update_timestep|max timesteps in one episode|int|4000|
+|--action_std|constant std for action distribution (Multivariate Normal)|float|default=0.5|
+|--K_epochs|update policy for K epochs|int|80|
+|--eps_clip|clip parameter for PPO|float|0.2|
+|--gamma|discount factor|float|0.99|
+|--lr|learning rate|float|0003|
+|--log_path|Tensorboard log path|str|tb_logs|
+|--model_path|Path for model persistence | str| models|
